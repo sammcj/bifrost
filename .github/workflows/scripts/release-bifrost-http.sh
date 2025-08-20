bb71745f7bd356c3c536c022df3dee608ce8ba86
Akshay Deo <akshay@akshaydeo.com>
2025-08-21 03:22:50 +0530

    adds skip-pipeline support as github bot check not working

--- .github/workflows/scripts/release-bifrost-http.sh
+++ .github/workflows/scripts/release-bifrost-http.sh
@@ -115,7 +115,7 @@ if ! git diff --quiet go.mod go.sum; then
   if [ ${#PLUGINS_USED[@]} -gt 0 ]; then
     commit_msg="$commit_msg, plugins: ${PLUGINS_USED[*]}"
   fi
-  git commit -m "$commit_msg"
+  git commit -m "$commit_msg --skip-pipeline"
   git push -u origin HEAD
   echo "✅ Transport dependencies updated"
 else
