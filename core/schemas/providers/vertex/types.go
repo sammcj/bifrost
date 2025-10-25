mbrukman/maximhq-bifrost

chore: restacked

core/schemas/providers/vertex/types.go
@@ -1,12 +1,14 @@
 package vertex
 
+import "time"
+
 // Vertex AI Embedding API types
 
 // VertexEmbeddingInstance represents a single embedding instance in the request
 type VertexEmbeddingInstance struct {
-	Content  string                   `json:"content"`             // The text to generate embeddings for
+	Content  string  `json:"content"`             // The text to generate embeddings for
 	TaskType *string `json:"task_type,omitempty"` // Intended downstream application (optional)
-	Title    *string                  `json:"title,omitempty"`     // Used to help the model produce better embeddings (optional)
+	Title    *string `json:"title,omitempty"`     // Used to help the model produce better embeddings (optional)
 }
 
 // VertexEmbeddingParameters represents the parameters for the embedding request
@@ -41,4 +43,20 @@ type VertexEmbeddingPrediction struct {
 // VertexEmbeddingResponse represents the complete embedding response from Vertex AI
 type VertexEmbeddingResponse struct {
 	Predictions []VertexEmbeddingPrediction `json:"predictions"` // List of embedding predictions
+}
+
+// ================================ Model Types ================================
+
+type VertexModel struct {
+	Name              string    `json:"name"`
+	VersionId         string    `json:"versionId"`
+	VersionAliases    []string  `json:"versionAliases"`
+	VersionCreateTime time.Time `json:"versionCreateTime"`
+	DisplayName       string    `json:"displayName"`
+	Description       string    `json:"description"`
+}
+
+type VertexListModelsResponse struct {
+	Models        []VertexModel `json:"models"`
+	NextPageToken string        `json:"next_page_token"`
 }

transports/bifrost-http/integrations/openai.go
@@ -101,7 +101,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/completions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -140,7 +140,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/chat/completions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -179,7 +179,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/responses",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -219,7 +219,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/embeddings",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -250,7 +250,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/audio/speech",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -286,7 +286,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/audio/transcriptions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -322,10 +322,74 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 	return routes
 }
 
+func CreateOpenAIListModelsRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) []RouteConfig {
+	var routes []RouteConfig
+
+	// Models endpoint
+	for _, path := range []string{
+		"/v1/models",
+		"/models",
+		"/openai/deployments/{deployment-id}/models",
+	} {
+		routes = append(routes, RouteConfig{
+			Type:   RouteConfigTypeOpenAI,
+			Path:   pathPrefix + path,
+			Method: "GET",
+			GetRequestTypeInstance: func() interface{} {
+				return &schemas.BifrostListModelsRequest{}
+			},
+			RequestConverter: func(req interface{}) (*schemas.BifrostRequest, error) {
+				if listModelsReq, ok := req.(*schemas.BifrostListModelsRequest); ok {
+					return &schemas.BifrostRequest{
+						ListModelsRequest: listModelsReq,
+					}, nil
+				}
+				return nil, errors.New("invalid request type")
+			},
+			ListModelsResponseConverter: func(resp *schemas.BifrostListModelsResponse) (interface{}, error) {
+				return openai.ToOpenAIListModelsResponse(resp), nil
+			},
+			ErrorConverter: func(err *schemas.BifrostError) interface{} {
+				return err
+			},
+			PreCallback: createOpenAIListModelsPreCallback(handlerStore),
+		})
+	}
+
+	return routes
+}
+
+// createOpenAIListModelsPreCallback creates a combined pre-callback for OpenAI list models
+// that handles both Azure endpoint preprocessing and query parameter extraction
+func createOpenAIListModelsPreCallback(handlerStore lib.HandlerStore) PreRequestCallback {
+	azureHook := AzureEndpointPreHook(handlerStore)
+
+	return func(ctx *fasthttp.RequestCtx, req interface{}) error {
+		// First run the Azure endpoint pre-hook if needed
+		if azureHook != nil {
+			if err := azureHook(ctx, req); err != nil {
+				return err
+			}
+		}
+
+		// Then extract query parameters for list models
+		if listModelsReq, ok := req.(*schemas.BifrostListModelsRequest); ok {
+			// Set provider to OpenAI (may be overridden by Azure hook)
+			if listModelsReq.Provider == "" {
+				listModelsReq.Provider = schemas.OpenAI
+			}
+
+			return nil
+		}
+
+		return nil
+	}
+}
+
 // NewOpenAIRouter creates a new OpenAIRouter with the given bifrost client.
 func NewOpenAIRouter(client *bifrost.Bifrost, handlerStore lib.HandlerStore, logger schemas.Logger) *OpenAIRouter {
 	return &OpenAIRouter{
-		GenericRouter: NewGenericRouter(client, handlerStore, CreateOpenAIRouteConfigs("/openai", handlerStore), logger),
+		GenericRouter: NewGenericRouter(client, handlerStore, append(CreateOpenAIRouteConfigs("/openai", handlerStore), CreateOpenAIListModelsRouteConfigs("/openai", handlerStore)...), logger),
 	}
 }
 

sammcj/bifrost

core/schemas/providers/vertex/types.go
@@ -1,12 +1,14 @@
 package vertex
 
+import "time"
+
 // Vertex AI Embedding API types
 
 // VertexEmbeddingInstance represents a single embedding instance in the request
 type VertexEmbeddingInstance struct {
-	Content  string                   `json:"content"`             // The text to generate embeddings for
+	Content  string  `json:"content"`             // The text to generate embeddings for
 	TaskType *string `json:"task_type,omitempty"` // Intended downstream application (optional)
-	Title    *string                  `json:"title,omitempty"`     // Used to help the model produce better embeddings (optional)
+	Title    *string `json:"title,omitempty"`     // Used to help the model produce better embeddings (optional)
 }
 
 // VertexEmbeddingParameters represents the parameters for the embedding request
@@ -41,4 +43,22 @@ type VertexEmbeddingPrediction struct {
 // VertexEmbeddingResponse represents the complete embedding response from Vertex AI
 type VertexEmbeddingResponse struct {
 	Predictions []VertexEmbeddingPrediction `json:"predictions"` // List of embedding predictions
+}
+
+// ================================ Model Types ================================
+
+const DefaultPageSize = 100
+
+type VertexModel struct {
+	Name              string    `json:"name"`
+	VersionId         string    `json:"versionId"`
+	VersionAliases    []string  `json:"versionAliases"`
+	VersionCreateTime time.Time `json:"versionCreateTime"`
+	DisplayName       string    `json:"displayName"`
+	Description       string    `json:"description"`
+}
+
+type VertexListModelsResponse struct {
+	Models        []VertexModel `json:"models"`
+	NextPageToken string        `json:"nextPageToken"`
 }

transports/bifrost-http/integrations/openai.go
@@ -55,6 +55,8 @@ func AzureEndpointPreHook(handlerStore lib.HandlerStore) func(ctx *fasthttp.Requ
 				r.Model = setAzureModelName(r.Model, deploymentIDStr)
 			case *openai.OpenAIEmbeddingRequest:
 				r.Model = setAzureModelName(r.Model, deploymentIDStr)
+			case *schemas.BifrostListModelsRequest:
+				r.Provider = schemas.Azure
 			}
 
 			if deploymentEndpoint == nil || azureKey == nil || !handlerStore.ShouldAllowDirectKeys() {
@@ -101,7 +103,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/completions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -140,7 +142,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/chat/completions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -179,7 +181,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/responses",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -219,7 +221,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/embeddings",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -250,7 +252,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/audio/speech",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -286,7 +288,7 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 		"/openai/deployments/{deployment-id}/audio/transcriptions",
 	} {
 		routes = append(routes, RouteConfig{
-			Type: RouteConfigTypeOpenAI,
+			Type:   RouteConfigTypeOpenAI,
 			Path:   pathPrefix + path,
 			Method: "POST",
 			GetRequestTypeInstance: func() interface{} {
@@ -322,10 +324,74 @@ func CreateOpenAIRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) 
 	return routes
 }
 
+func CreateOpenAIListModelsRouteConfigs(pathPrefix string, handlerStore lib.HandlerStore) []RouteConfig {
+	var routes []RouteConfig
+
+	// Models endpoint
+	for _, path := range []string{
+		"/v1/models",
+		"/models",
+		"/openai/deployments/{deployment-id}/models",
+	} {
+		routes = append(routes, RouteConfig{
+			Type:   RouteConfigTypeOpenAI,
+			Path:   pathPrefix + path,
+			Method: "GET",
+			GetRequestTypeInstance: func() interface{} {
+				return &schemas.BifrostListModelsRequest{}
+			},
+			RequestConverter: func(req interface{}) (*schemas.BifrostRequest, error) {
+				if listModelsReq, ok := req.(*schemas.BifrostListModelsRequest); ok {
+					return &schemas.BifrostRequest{
+						ListModelsRequest: listModelsReq,
+					}, nil
+				}
+				return nil, errors.New("invalid request type")
+			},
+			ListModelsResponseConverter: func(resp *schemas.BifrostListModelsResponse) (interface{}, error) {
+				return openai.ToOpenAIListModelsResponse(resp), nil
+			},
+			ErrorConverter: func(err *schemas.BifrostError) interface{} {
+				return err
+			},
+			PreCallback: setQueryParamsAndAzureEndpointPreHook(handlerStore),
+		})
+	}
+
+	return routes
+}
+
+// setQueryParamsAndAzureEndpointPreHook creates a combined pre-callback for OpenAI list models
+// that handles both Azure endpoint preprocessing and query parameter extraction
+func setQueryParamsAndAzureEndpointPreHook(handlerStore lib.HandlerStore) PreRequestCallback {
+	azureHook := AzureEndpointPreHook(handlerStore)
+
+	return func(ctx *fasthttp.RequestCtx, req interface{}) error {
+		// First run the Azure endpoint pre-hook if needed
+		if azureHook != nil {
+			if err := azureHook(ctx, req); err != nil {
+				return err
+			}
+		}
+
+		// Then extract query parameters for list models
+		if listModelsReq, ok := req.(*schemas.BifrostListModelsRequest); ok {
+			// Set provider to OpenAI (may be overridden by Azure hook)
+			if listModelsReq.Provider == "" {
+				listModelsReq.Provider = schemas.OpenAI
+			}
+
+			return nil
+		}
+
+		return nil
+	}
+}
+
 // NewOpenAIRouter creates a new OpenAIRouter with the given bifrost client.
 func NewOpenAIRouter(client *bifrost.Bifrost, handlerStore lib.HandlerStore, logger schemas.Logger) *OpenAIRouter {
 	return &OpenAIRouter{
-		GenericRouter: NewGenericRouter(client, handlerStore, CreateOpenAIRouteConfigs("/openai", handlerStore), logger),
+		GenericRouter: NewGenericRouter(client, handlerStore, append(CreateOpenAIRouteConfigs("/openai", handlerStore), CreateOpenAIListModelsRouteConfigs("/openai", handlerStore)...), logger),
 	}
 }
 