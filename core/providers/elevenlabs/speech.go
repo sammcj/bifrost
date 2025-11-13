775922c254517cb0e94067df651f08c624034a42
Samyabrata Maji <samyabratamaji334@gmail.com>
2025-11-13 11:09:09 +0530

    feat: Speech provider for elevenlabs

---
+++ core/providers/elevenlabs/speech.go
@@ -0,0 +1,94 @@
+package elevenlabs
+
+import (
+	"github.com/maximhq/bifrost/core/schemas"
+)
+
+func ToElevenlabsSpeechRequest(bifrostReq *schemas.BifrostSpeechRequest) *ElevenlabsSpeechRequest {
+	if bifrostReq == nil {
+		return nil
+	}
+
+	elevenlabsReq := &ElevenlabsSpeechRequest{
+		ModelID: schemas.Ptr(bifrostReq.Model),
+		Text:    bifrostReq.Input.Input,
+	}
+
+	if bifrostReq.Params != nil {
+		voiceSettings := ElevenLabsVoiceSettings{}
+		hasVoiceSettings := false
+
+		if bifrostReq.Params.Speed != nil {
+			voiceSettings.Speed = *bifrostReq.Params.Speed
+			hasVoiceSettings = true
+		}
+		if bifrostReq.Params.Stability != nil {
+			voiceSettings.Stability = *bifrostReq.Params.Stability
+			hasVoiceSettings = true
+		}
+		if bifrostReq.Params.UseSpeakerBoost != nil {
+			voiceSettings.UseSpeakerBoost = *bifrostReq.Params.UseSpeakerBoost
+			hasVoiceSettings = true
+		}
+		if bifrostReq.Params.SimilarityBoost != nil {
+			voiceSettings.SimilarityBoost = *bifrostReq.Params.SimilarityBoost
+			hasVoiceSettings = true
+		}
+		if bifrostReq.Params.Style != nil {
+			voiceSettings.Style = *bifrostReq.Params.Style
+			hasVoiceSettings = true
+		}
+		if hasVoiceSettings {
+			elevenlabsReq.VoiceSettings = &voiceSettings
+		}
+
+		if bifrostReq.Params.LanguageCode != nil {
+			elevenlabsReq.LanguageCode = bifrostReq.Params.LanguageCode
+		}
+
+		if bifrostReq.Params.Seed != nil {
+			elevenlabsReq.Seed = bifrostReq.Params.Seed
+		}
+
+		if len(bifrostReq.Params.PronunciationDictionaryLocators) > 0 {
+			elevenlabsReq.PronunciationDictionaryLocators = make([]ElevenlabsPronunciationDictionaryLocator, len(bifrostReq.Params.PronunciationDictionaryLocators))
+			for i, locator := range bifrostReq.Params.PronunciationDictionaryLocators {
+				elevenlabsReq.PronunciationDictionaryLocators[i] = ElevenlabsPronunciationDictionaryLocator{
+					PronunciationDictionaryID: locator.PronunciationDictionaryID,
+					VersionID:                 locator.VersionID,
+				}
+			}
+		}
+
+		if bifrostReq.Params.PreviousText != nil {
+			elevenlabsReq.PreviousText = bifrostReq.Params.PreviousText
+		}
+
+		if bifrostReq.Params.NextText != nil {
+			elevenlabsReq.NextText = bifrostReq.Params.NextText
+		}
+
+		if bifrostReq.Params.PreviousRequestIDs != nil {
+			elevenlabsReq.PreviousRequestIDs = bifrostReq.Params.PreviousRequestIDs
+		}
+
+		if bifrostReq.Params.NextRequestIDs != nil {
+			elevenlabsReq.NextRequestIDs = bifrostReq.Params.NextRequestIDs
+		}
+
+		if bifrostReq.Params.ApplyTextNormalization != nil {
+			tn := TextNormalization(*bifrostReq.Params.ApplyTextNormalization)
+			elevenlabsReq.ApplyTextNormalization = &tn
+		}
+
+		if bifrostReq.Params.ApplyLanguageTextNormalization != nil {
+			elevenlabsReq.ApplyLanguageTextNormalization = bifrostReq.Params.ApplyLanguageTextNormalization
+		}
+
+		if bifrostReq.Params.UsePVCAsIVC != nil {
+			elevenlabsReq.UsePVCAsIVC = bifrostReq.Params.UsePVCAsIVC
+		}
+	}
+
+	return elevenlabsReq
+}
--- core/schemas/speech.go
+++ core/schemas/speech.go
@@ -36,10 +36,70 @@ type SpeechParameters struct {
 	ResponseFormat string            `json:"response_format,omitempty"` // Default is "mp3"
 	Speed          *float64          `json:"speed,omitempty"`
 
+	// Elevenlabs-specific fields
+	Stability                       *float64                               `json:"stability,omitempty"`
+	UseSpeakerBoost                 *bool                                  `json:"use_speaker_boost,omitempty"`
+	SimilarityBoost                 *float64                               `json:"similarity_boost,omitempty"`
+	Style                           *float64                               `json:"style,omitempty"`
+	LanguageCode                    *string                                `json:"language_code,omitempty"`
+	Seed                            *int                                   `json:"seed,omitempty"`
+	PronunciationDictionaryLocators []SpeechPronunciationDictionaryLocator `json:"pronunciation_dictionary_locators"`
+	PreviousText                    *string                                `json:"previous_text,omitempty"`
+	NextText                        *string                                `json:"next_text,omitempty"`
+	PreviousRequestIDs              []string                               `json:"previous_request_ids"`
+	NextRequestIDs                  []string                               `json:"next_request_ids"`
+	ApplyTextNormalization          *SpeechTextNormalization               `json:"apply_text_normalization,omitempty"`
+	ApplyLanguageTextNormalization  *bool                                  `json:"apply_language_text_normalization,omitempty"`
+	UsePVCAsIVC                     *bool                                  `json:"use_pvc_as_ivc,omitempty"`
+
+	// Elevenlabs-specific query parameters
+	EnableLogging            *bool               `json:"enable_logging,omitempty"`
+	OptimizeStreamingLatency *bool               `json:"optimize_streaming_latency,omitempty"`
+	OutputFormat             *SpeechOutputFormat `json:"output_format,omitempty"`
+
 	// Dynamic parameters that can be provider-specific, they are directly
 	// added to the request as is.
 	ExtraParams map[string]interface{} `json:"-"`
 }
+
+type SpeechOutputFormat string
+
+const (
+	SpeechOutputFormatMP3_22050_32   SpeechOutputFormat = "mp3_22050_32"
+	SpeechOutputFormatMP3_24000_48   SpeechOutputFormat = "mp3_24000_48"
+	SpeechOutputFormatMP3_44100_32   SpeechOutputFormat = "mp3_44100_32"
+	SpeechOutputFormatMP3_44100_64   SpeechOutputFormat = "mp3_44100_64"
+	SpeechOutputFormatMP3_44100_96   SpeechOutputFormat = "mp3_44100_96"
+	SpeechOutputFormatMP3_44100_128  SpeechOutputFormat = "mp3_44100_128"
+	SpeechOutputFormatMP3_44100_192  SpeechOutputFormat = "mp3_44100_192"
+	SpeechOutputFormatPCM_8000       SpeechOutputFormat = "pcm_8000"
+	SpeechOutputFormatPCM_16000      SpeechOutputFormat = "pcm_16000"
+	SpeechOutputFormatPCM_22050      SpeechOutputFormat = "pcm_22050"
+	SpeechOutputFormatPCM_24000      SpeechOutputFormat = "pcm_24000"
+	SpeechOutputFormatPEM_32000      SpeechOutputFormat = "pem_32000"
+	SpeechOutputFormatPCM_44100      SpeechOutputFormat = "pcm_44100"
+	SpeechOutputFormatPCM_48000      SpeechOutputFormat = "pcm_48000"
+	SpeechOutputFormatULAW_8000      SpeechOutputFormat = "ulaw_8000"
+	SpeechOutputFormatALAW_8000      SpeechOutputFormat = "alaw_8000"
+	SpeechOutputFormatOpus_48000_32  SpeechOutputFormat = "opus_48000_32"
+	SpeechOutputFormatOpus_48000_64  SpeechOutputFormat = "opus_48000_64"
+	SpeechOutputFormatOpus_48000_96  SpeechOutputFormat = "opus_48000_96"
+	SpeechOutputFormatOpus_48000_128 SpeechOutputFormat = "opus_48000_128"
+	SpeechOutputFormatOpus_48000_192 SpeechOutputFormat = "opus_48000_192"
+)
+
+type SpeechPronunciationDictionaryLocator struct {
+	PronunciationDictionaryID string  `json:"pronunciation_dictionary_id"`
+	VersionID                 *string `json:"version_id,omitempty"`
+}
+
+type SpeechTextNormalization string
+
+const (
+	SpeechTextNormalizationAuto SpeechTextNormalization = "auto"
+	SpeechTextNormalizationOn   SpeechTextNormalization = "on"
+	SpeechTextNormalizationOff  SpeechTextNormalization = "off"
+)
 
 type SpeechVoiceInput struct {
 	Voice            *string
