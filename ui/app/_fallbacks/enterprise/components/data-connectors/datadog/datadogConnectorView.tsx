4afc224f4a761711041a2fe56db696c3e589ca22
akshaydeo <akshay@akshaydeo.com>
2026-01-19 22:30:16 +0530

    datadog docs

--- ui/app/_fallbacks/enterprise/components/data-connectors/datadog/datadogConnectorView.tsx
+++ ui/app/_fallbacks/enterprise/components/data-connectors/datadog/datadogConnectorView.tsx
@@ -10,7 +10,7 @@ export default function DatadogConnectorView() {
 				icon={<Dog className="h-[5.5rem] w-[5.5rem]" strokeWidth={1} />}
 				title="Unlock native Datadog data ingestion for better observability"
 				description="This feature is a part of the Bifrost enterprise license. We would love to know more about your use case and how we can help you."
-				readmeLink="https://docs.getbifrost.ai/enterprise/data-connectors/datadog"
+				readmeLink="https://docs.getbifrost.ai/enterprise/datadog-connector"
 			/>
 		</div>
 	);
